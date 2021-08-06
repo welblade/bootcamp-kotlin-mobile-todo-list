66eb505b471dd583b89df6e1bff30a08c93c6adf
Wellington Oliveira dos Santos <welblade@gmail.com>
2021-08-06 19:41:42 -0300

    * Adicionado adapter para a lista de tarefas

--- app/src/main/java/com/github/welblade/todolist/ui/MainActivity.kt
+++ app/src/main/java/com/github/welblade/todolist/ui/MainActivity.kt
@@ -9,10 +9,16 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private val adapter: TaskListAdapter by lazy { TaskListAdapter() }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        initTaskList()
         insertListeners()
+    }
+
+    private fun initTaskList(){
+        binding.rvTaskList.adapter = adapter
     }
 
     private fun insertListeners() {
