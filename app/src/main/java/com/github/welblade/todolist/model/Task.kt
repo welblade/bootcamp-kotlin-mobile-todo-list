5dcc0cd4a5f4f7e1690647babeb3b7080ac4f39d
Wellington Oliveira dos Santos <welblade@gmail.com>
2021-08-10 13:01:41 -0300

    * Adicionado campo de descrição de tarefa

--- app/src/main/java/com/github/welblade/todolist/model/Task.kt
+++ app/src/main/java/com/github/welblade/todolist/model/Task.kt
@@ -2,6 +2,7 @@ package com.github.welblade.todolist.model
 
 data class Task(
     val title: String,
+    val description: String,
     val date: String,
     val hour: String,
     val id: Int = 0
